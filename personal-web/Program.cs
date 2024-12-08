e4e3876d915101927b6b0255df9a99918b94d3a1
Ruben Escudero <rubenescudero00@outlook.com>
2024-12-08 19:44:02 +0100

    crear primer menu

--- personal-web/Program.cs
+++ personal-web/Program.cs
@@ -1,6 +1,6 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using personal_web;
+using PersonalWeb;
 using Radzen;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
--- personal-web/obj/Debug/net8.0/PersonalWeb.AssemblyInfo.cs
+++ personal-web/obj/Debug/net8.0/PersonalWeb.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("personal-web")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("PersonalWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f43d4ff50ff894b299013f83a095349cc24e3fae")]
-[assembly: System.Reflection.AssemblyProductAttribute("personal-web")]
-[assembly: System.Reflection.AssemblyTitleAttribute("personal-web")]
+[assembly: System.Reflection.AssemblyProductAttribute("PersonalWeb")]
+[assembly: System.Reflection.AssemblyTitleAttribute("PersonalWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Generado por la clase WriteCodeFragment de MSBuild.
